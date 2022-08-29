de912d3680eeb8f5950789158a6a5fedadc36345
ingridmolina <ingridmolina900@gmail.com>
2022-08-29 21:43:00 +0200

    primer commit

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,16 @@
+/*licencias de uso*/
+
+package com.mycompany.holamundo;
+
+/*Author@molina
+
+/**
+ *
+ * @author molina
+ */
+public class Holamundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
